36cdb5f1bb62c8b447c2fecfcb00d435f28d1220
Jelte Fennema <jelte12345@gmail.com>
2013-11-09 17:24:34 +0100

    Fix caching mistake by adding __fish_service_print_names from the default service.fish file

--- completions/rc-service.fish
+++ completions/rc-service.fish
@@ -1,3 +1,11 @@
+function __fish_service_print_names
+	if type -f systemctl >/dev/null
+        command systemctl list-units  -t service | cut -d ' ' -f 1 | grep '\.service$' | sed -e 's/\.service$//'
+    end
+
+    command ls /etc/init.d
+end
+
 # Fist argument is the names of the service, i.e. a file in /etc/init.d
 complete -c rc-service -n "test (count (commandline -poc)) = 1" -xa "(__fish_service_print_names)" --description "Service name"
 
