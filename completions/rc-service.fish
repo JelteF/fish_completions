9127b8f267cea3151893ed9433df194f436ccdff
Jelte Fennema <jelte12345@gmail.com>
2013-11-09 21:08:20 +0100

    Fix option bug and add option completions for rc-service

--- completions/rc-service.fish
+++ completions/rc-service.fish
@@ -3,11 +3,22 @@ function __fish_service_print_names
         command systemctl list-units  -t service | cut -d ' ' -f 1 | grep '\.service$' | sed -e 's/\.service$//'
     end
 
-    command ls /etc/init.d
+    command rc-service -l
 end
 
 # Fist argument is the names of the service, i.e. a file in /etc/init.d
-complete -c rc-service -n "test (count (commandline -poc)) = 1" -xa "(__fish_service_print_names)" --description "Service name"
+complete -c rc-service -n "test (count (__fish_print_cmd_args_without_options)) = 1" -xa "(__fish_service_print_names)" --description "Service name"
 
-#The second argument is what action to take with the service
-complete -c rc-service -n "test (count (commandline -poc)) -gt 1" -xa "(__fish_complete_service_actions)"
+# The second argument is what action to take with the service
+complete -c rc-service -n "test (count (__fish_print_cmd_args_without_options)) -gt 1" -xa "(__fish_complete_service_actions)"
+
+# Complete rc-service the options
+complete -c rc-service -s e -l exists -d 'Tests if the service exists or not'
+complete -c rc-service -s i -l ifexists -d 'If the service exists, then run the command'
+complete -c rc-service -s l -l list -d 'List all available services'
+complete -c rc-service -s r -l resolve -d 'Resolve the service name to an init script'
+complete -c rc-service -s h -l help -d 'Display the help output'
+complete -c rc-service -s C -l nocolor -d 'Disable color output'
+complete -c rc-service -s V -l version -d 'Display software version'
+complete -c rc-service -s v -l verbose -d 'Run verbosely'
+complete -c rc-service -s q -l quiet -d 'Run quietly (Does not affect errors)'
