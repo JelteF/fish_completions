cffd2fabc0fd57002891343036551ad719e4d26a
Jelte Fennema <jelte12345@gmail.com>
2013-11-19 19:22:22 +0100

    Add --color=no to eselect completions

--- completions/eselect.fish
+++ completions/eselect.fish
@@ -1,12 +1,14 @@
+set __fish_eselect_cmd eselect --brief --colour=no
+
 function __fish_complete_eselect_modules
     set -l sedregexp 's/^  ([a-zA-Z0-9_-]*)[ ]*/\1\t/g'
-    eselect --brief modules list | grep '^  ' | sed -r $sedregexp
+    eval $__fish_eselect_cmd modules list | grep '^  ' | sed -r $sedregexp
 end
 
 function __fish_complete_eselect_actions
     set -l sedregexp 's/^  ([a-zA-Z0-9_-]*)[ ]*/\1\t/g'
     set -l cmdl (commandline -poc)
-    eval $cmdl[1] --brief $cmdl[2..-1] help | grep '^  [^ -]' | sed -r $sedregexp
+    eval $__fish_eselect_cmd $cmdl[2..-1] help | grep '^  [^ -]' | sed -r $sedregexp
 end
 
 function __fish_complete_eselect_action_options
@@ -21,7 +23,7 @@ function __fish_complete_eselect_action_options
     set -l findregexp '/^  '$cmdl[-1]'/,/^  [^ ]/p'
 
     set cmdl[-1] help
-    eval $cmdl[1] --brief $cmdl[2..-1] | sed -n -re $findregexp | grep '^    --' | sed -re $parseregexp
+    eval $__fish_eselect_cmd $cmdl[2..-1] | sed -n -re $findregexp | grep '^    --' | sed -re $parseregexp
 end
 
 function __fish_complete_eselect_targets
@@ -35,7 +37,7 @@ function __fish_complete_eselect_targets
             set cmdl[-1] list
     end
 
-    eval $cmdl[1] $cmdl[2..-1] | grep '^  [^ -]' | sed -r $sedregexp
+    eval $cmdl[1] --colour=no $cmdl[2..-1] | grep '^  [^ -]' | sed -r $sedregexp
 end
 
 complete -c eselect -n "test (__fish_number_of_cmd_args_wo_opts) = 1" \
